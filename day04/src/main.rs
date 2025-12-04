ada3edc5e92c7c271ffacf51506fd429961cc4e7
Travis Parks <tparks@pinnacle21.com>
2025-12-04 11:47:17 -0500

    Day 04 - Find rolls that can be moved

---
+++ day04/src/main.rs
@@ -0,0 +1,200 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+#[derive(Debug)]
+struct AdventError {
+    message: String,
+}
+
+impl AdventError {
+    #[inline]
+    #[must_use]
+    pub fn new(message: impl Into<String>) -> Self {
+        Self {
+            message: message.into(),
+        }
+    }
+}
+
+impl Display for AdventError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for AdventError {}
+
+fn main() -> Result<(), AdventError> {
+    let path = Path::new("day04/resources/input.txt");
+    run_first_part(path)?;
+    run_second_part(path)?;
+    Ok(())
+}
+
+fn run_first_part(path: &Path) -> Result<(), AdventError> {
+    let rolls = match read_roll_lines(path) {
+        Ok(rolls) => rolls,
+        Err(e) => return Err(e),
+    };
+    let accessible_rolls = count_accessible_rolls(&rolls);
+    println!("Part 1 - There are {accessible_rolls} accessible roll(s)");
+    Ok(())
+}
+
+fn count_accessible_rolls(rolls: &Vec<Vec<bool>>) -> u32 {
+    let mut accessible_rolls = 0u32;
+    for (row_index, row) in rolls.iter().enumerate() {
+        for (column_index, column) in row.iter().enumerate() {
+            if !*column {
+                continue;
+            }
+            let occupied_count = count_adjacent_rolls(rolls, row_index, column_index);
+            if occupied_count < 4 {
+                accessible_rolls += 1;
+            }
+        }
+    }
+    accessible_rolls
+}
+
+fn run_second_part(path: &Path) -> Result<(), AdventError> {
+    let mut rolls = match read_roll_lines(path) {
+        Ok(rolls) => rolls,
+        Err(e) => return Err(e),
+    };
+    let moved_rolls = count_accessible_rolls_repeatedly(&mut rolls);
+    println!("Part 2 - There are {moved_rolls} moved roll(s)");
+    Ok(())
+}
+
+fn count_accessible_rolls_repeatedly(rolls: &mut Vec<Vec<bool>>) -> usize {
+    let mut moved_rolls = 0usize;
+    loop {
+        let accessible_rolls = find_accessible_rolls(&rolls);
+        let accessible_roll_count = accessible_rolls.len();
+        if accessible_roll_count == 0 {
+            break;
+        }
+        moved_rolls += accessible_roll_count;
+        for (row_index, column_index) in accessible_rolls {
+            rolls[row_index][column_index] = false;
+        }
+    }
+    moved_rolls
+}
+
+fn find_accessible_rolls(rolls: &Vec<Vec<bool>>) -> Vec<(usize, usize)> {
+    let mut accessible_rolls = Vec::new();
+    for (row_index, row) in rolls.iter().enumerate() {
+        for (column_index, column) in row.iter().enumerate() {
+            if !*column {
+                continue;
+            }
+            let occupied_count = count_adjacent_rolls(rolls, row_index, column_index);
+            if occupied_count < 4 {
+                accessible_rolls.push((row_index, column_index));
+            }
+        }
+    }
+    accessible_rolls
+}
+
+fn count_adjacent_rolls(rolls: &Vec<Vec<bool>>, row_index: usize, column_index: usize) -> u32 {
+    let row = &rolls[row_index];
+    let mut occupied_count = 0u32;
+    let start_row = row_index as isize - 1;
+    let end_row = row_index as isize + 1;
+    let start_column = column_index as isize - 1;
+    let end_column = column_index as isize + 1;
+    for adjacent_row_index in start_row..=end_row {
+        // Ignore rows that are out of bounds.
+        if adjacent_row_index < 0 || adjacent_row_index >= rolls.len() as isize {
+            continue;
+        }
+        for adjacent_column_index in start_column..=end_column {
+            // Ignore columns that are out of bounds.
+            if adjacent_column_index < 0 || adjacent_column_index >= row.len() as isize {
+                continue;
+            }
+            // Don't look at the current value.
+            if adjacent_row_index == row_index as isize
+                && adjacent_column_index == column_index as isize
+            {
+                continue;
+            }
+            if rolls[adjacent_row_index as usize][adjacent_column_index as usize] {
+                occupied_count += 1;
+            }
+        }
+    }
+    occupied_count
+}
+
+fn read_roll_lines(path: &Path) -> Result<Vec<Vec<bool>>, AdventError> {
+    let Ok(file) = File::open(path) else {
+        return Err(AdventError::new("Could not open the input file"));
+    };
+    let reader = BufReader::new(file);
+    let mut rolls = Vec::new();
+    for line in reader.lines() {
+        let Ok(line) = line else {
+            return Err(AdventError::new("Could not read the next line"));
+        };
+        let mut roll_line = Vec::new();
+        for next in line.chars() {
+            let is_roll = match next {
+                '.' => false,
+                '@' => true,
+                _ => return Err(AdventError::new("Encountered an invalid character")),
+            };
+            roll_line.push(is_roll);
+        }
+        rolls.push(roll_line);
+    }
+    Ok(rolls)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{count_accessible_rolls, count_accessible_rolls_repeatedly};
+
+    #[test]
+    fn test_part1_example() {
+        let rolls = read_test_data();
+        let count = count_accessible_rolls(&rolls);
+        assert_eq!(13, count);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let mut rolls = read_test_data();
+        let count = count_accessible_rolls_repeatedly(&mut rolls);
+        assert_eq!(43, count);
+    }
+
+    fn read_test_data() -> Vec<Vec<bool>> {
+        let raw_data = "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
+        let mut rolls = Vec::new();
+        for line in raw_data.lines() {
+            let mut roll_line = Vec::new();
+            for char in line.chars() {
+                let is_roll = char == '@';
+                roll_line.push(is_roll);
+            }
+            rolls.push(roll_line);
+        }
+        rolls
+    }
+}
